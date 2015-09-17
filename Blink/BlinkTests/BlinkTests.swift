67488d46630b149118d41568458f94e480c3d91b
rémi  <remirobert33530@gmail.com>
2015-09-17 12:26:13 +0800

    add startup project

---
+++ Blink/BlinkTests/BlinkTests.swift
@@ -0,0 +1,36 @@
+//
+//  BlinkTests.swift
+//  BlinkTests
+//
+//  Created by Remi Robert on 17/09/15.
+//  Copyright © 2015 Remi Robert. All rights reserved.
+//
+
+import XCTest
+@testable import Blink
+
+class BlinkTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}

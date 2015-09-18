3e34aa9aee9dcad8ca6cc4fbe755497108ad6bd3
rémi  <remirobert33530@gmail.com>
2015-09-18 16:00:34 +0800

    add fetch user root api

---
+++ Blink/API/Room.swift
@@ -0,0 +1,34 @@
+//
+//  Room.swift
+//  Blink
+//
+//  Created by Remi Robert on 18/09/15.
+//  Copyright © 2015 Remi Robert. All rights reserved.
+//
+
+import UIKit
+import Parse
+import ReactiveCocoa
+
+class Room {
+
+    class func fetchDirectRoomParticipantUser() -> RACSignal {
+        return RACSignal.createSignal({ (subscriber: RACSubscriber!) -> RACDisposable! in
+            
+            let querryParticipant = PFQuery(className: "Participant")
+            
+            querryParticipant.whereKey("user", equalTo: PFUser.currentUser()!)
+            querryParticipant.findObjectsInBackgroundWithBlock({ (results: [PFObject]?, error: NSError?) -> Void in
+                if let participants = results where error == nil {
+                    let rooms = participants.flatMap { ($0["room"]) }
+                    subscriber.sendNext(rooms)
+                    subscriber.sendCompleted()
+                }
+                else {
+                    subscriber.sendError(error)
+                }
+            })
+            return nil
+        })
+    }
+}
